de1ff913e4f0f493f8b420162da331213d8313fb
jaypowar00 <jaypowar00@gmail.com>
2020-11-29 12:57:58 +0530

    updated allowed host

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -78,10 +78,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecommerce_api.wsgi.application'
 
 # Database
-try:
-    from ecommerce_api.local_settings import *
-except ImportError:
-    pass
+# try:
+#     from ecommerce_api.local_settings import *
+# except ImportError:
+#     pass
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd52la22ds53ipk',  # 'ecommerce_api',
+        'PORT': '5432',
+        'HOST': 'ec2-52-86-116-94.compute-1.amazonaws.com',  # 'localhost',
+        'USER': 'vmkijycgpkvvqh',  # 'postgres',
+        'PASSWORD': '3e7d1b2695035d8429ec57a3397328adeaf0ec0eca4bc628839c40096c563b48',  # 'super'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
