a5b6da4b3361007bbb534254e39188d2d1861b2a
jaypowar00 <jaypowar00@gmail.com>
2020-11-29 12:53:45 +0530

    updated allowed host

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'e-cmrc-stackunderflow-api.herokuapp.co',
+    'e-cmrc-stackunderflow-api.herokuapp.com',
 ]
 blackListedTokens = set()
 # Application definition
