da4af3622cd4366461c1bfda2ab4a3ae0d137126
jaypowar00 <jaypowar00@gmail.com>
2020-11-29 12:53:25 +0530

    added procfile

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'https://e-cmrc-stackunderflow-api.herokuapp.com/',
+    'e-cmrc-stackunderflow-api.herokuapp.com',
 ]
 blackListedTokens = set()
 # Application definition
