a2519f941ced43f1c755ea070796f639bca8e6c6
jaypowar00 <jaypowar00@gmail.com>
2020-12-29 19:34:08 +0530

    static file settings bug fix

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'local_settings.py')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'user.Details'
 
